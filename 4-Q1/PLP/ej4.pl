04cb4285708603059bc9106dc2851f2825dc03d0
GonzaloAgu <gonzalo.ag88@gmail.com>
2026-03-28 11:14:38 -0300

    ej4 corregido

--- 4-Q1/PLP/ej4.pl
+++ 4-Q1/PLP/ej4.pl
@@ -1,13 +1,13 @@
-
+:- use_module(library(clpfd)).
 
 result(A1, A2, A3, B1, B3, C1, C2, C3) :-
-    permutation([0,1,2,3,4,5,6,7,8,9], [A1,A2,A3,B1,B3,C1,C2,C3|_]),
+    [A1, A2, A3, B1, B3, C1, C2, C3] ins 0..9,
     
     A1 * A2 * A3 #= 6,
     (B1 * 5) - B3  #= 1,
     (C1 - C2) * C3 #= 4,
     (A1 - B1) * C1 #= 7,
-    (A2 + 5)    #= C2,       
+    A2 + 5 #= C2,          % reescrita sin división
     (A3 + B3) - C3 #= 7,
-    
-    label(Vars).
+    s
+    label([A1, A2, A3, B1, B3, C1, C2, C3]).
