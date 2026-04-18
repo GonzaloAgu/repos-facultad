7afeb691922ff1902c19cf05006e2972f065e86a
GonzaloAgu <gonzalo.ag88@gmail.com>
2026-04-18 20:37:51 -0300

    ej13

--- 4-Q1/PLP/Haskell/ej12.hs
+++
@@ -1,4 +0,0 @@
-{--
-Analizar la función (.) definida en Prelude, definir su objetivo y determinar si
-los ejemplos de aplicación son correctos o no.
---}
--- 4-Q1/PLP/Haskell/ej13.hs
+++ 4-Q1/PLP/Haskell/ej13.hs
@@ -1,6 +1,21 @@
+{- HLINT ignore "Redundant bracket" -}
+import Data.Char
 {--
 Definir las funciones:
 - sin_consonantes: dada una lista de palabras retorna la lista con las palabras sin sus consonantes.
 - cuantas_consonantes: dada una lista de palabras retorna la lista con la cantidad de consonantes de cada palabra.
 - h: dada una lista de palabras y una función, puede comportarse como las anteriores.
 --}
+{- HLINT ignore "Use infix" -}
+esConsonante :: Char -> Bool
+esConsonante char = notElem (toLower char) ['a', 'e', 'i', 'o', 'u']
+
+quitarConsonantes :: String -> String
+quitarConsonantes [] = []
+quitarConsonantes (primero:resto) = 
+    if esConsonante primero then quitarConsonantes resto
+    else (primero:quitarConsonantes resto)
+
+
+sinConsonantes :: [String] -> [String]
+sinConsonantes lista_strings =  map quitarConsonantes lista_strings
