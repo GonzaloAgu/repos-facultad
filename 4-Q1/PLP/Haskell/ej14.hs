41a99177186601b00727cfae92102af7e9cabaa0
GonzaloAgu <gonzalo.ag88@gmail.com>
2026-04-21 21:26:47 -0300

    ej14-15

--- 4-Q1/PLP/Haskell/ej14.hs
+++ 4-Q1/PLP/Haskell/ej14.hs
@@ -1,6 +1,11 @@
 {--
 Dadas las siguientes funciones, determinar su comportamiento ante las 
 invocaciones y el concepto que se aplica:
+--}
+
 ignoroArg1 x = "No me importa"
 ignoroArg2 x = x
---}
+
+res = ignoroArg1 (1/0)
+
+-- Se aplica el concepto de evaluación perezosa: los argumentos no se evalúan si no es necesario.
--- 4-Q1/PLP/Haskell/ej15.hs
+++ 4-Q1/PLP/Haskell/ej15.hs
@@ -1,14 +1,18 @@
 {--
 Analizar las siguientes funciones, definir su objetivo y determinar los 
 conceptos que se aplican:
+--}
+
 queHace :: [Int] -> Int
 queHace [] = error "lista vacía!"
 queHace [x] = x
 queHace (x:y:ys) = queHace (queHaceAux1 x y : ys)
+
 queHaceAux1::Int->Int->Int
 queHaceAux1 x y |x>y = queHaceAux2 y [x,x+x ..]
  |otherwise = queHaceAux2 x [y,y+y .. ]
+
+ 
 queHaceAux2 :: Int -> [Int] -> Int
 queHaceAux2 x (y:ys) |mod y x == 0 = y
  |otherwise = queHaceAux2 x ys
---}
