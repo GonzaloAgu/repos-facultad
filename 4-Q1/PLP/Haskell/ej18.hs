f3b58ff4ddf64cdd1e742b49ef24d18cdae1288e
Gonzalo Agu <gonzalo.ag88@gmail.com>
2026-04-23 16:06:41 -0400

    wip ej19

--- 4-Q1/PLP/Haskell/ej18.hs
+++ 4-Q1/PLP/Haskell/ej18.hs
@@ -1,4 +1,12 @@
 {--
-Definir la función extremo tal que dado un criterio de selección y una lista 
-retorna el elemento de la lista que verifica el criterio.
+    Definir la función extremo tal que dado un criterio de selección y una lista 
+    retorna el elemento de la lista que verifica el criterio.
 --}
+
+extremo :: (a -> a -> Bool) -> [a] -> a
+extremo _ [] = error "nooo"
+extremo f [a] = a
+
+extremo f (x:y:resto)
+    | f x y = extremo f (x:resto)
+    | otherwise = extremo f (y:resto)
--- 4-Q1/PLP/Haskell/ej19.hs
+++ 4-Q1/PLP/Haskell/ej19.hs
@@ -1,4 +1,19 @@
 {--
-Definir la función ordenar tal que dado un criterio y una lista retorna la lista 
-ordenada según el criterio.
+    Definir la función ordenar tal que dado un criterio y una lista retorna la lista 
+    ordenada según el criterio.
 --}
+
+ordenar f lista
+    | ordenada lista = listArray
+    | otherwise ordenar burbuja lista
+
+ordenada :: [a] -> Bool
+ordenada lista = True
+
+burbuja :: (a->a->Bool) -> [a] -> [a]
+burbuja _ [] = []
+burbuja _ [x] = [x]
+
+burbuja f (x:y:resto)
+    | f x y = (x:burbuja f (y:resto))
+    | otherwise = (y:burbuja f (x:resto))
