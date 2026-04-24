4529974c76ddfcc36fe7e9de95bb5198e2e14d7d
Gonzalo Agu <gonzaloagu99@gmail.com>
2026-04-24 18:00:52 -0300

    funcion ordenar

--- 4-Q1/PLP/Haskell/ej19.hs
+++ 4-Q1/PLP/Haskell/ej19.hs
@@ -4,16 +4,18 @@
 --}
 
 ordenar f lista
-    | ordenada lista = listArray
-    | otherwise ordenar burbuja lista
+    | ordenada f lista = lista
+    | otherwise = ordenar f (burbuja f lista)
 
-ordenada :: [a] -> Bool
-ordenada lista = True
+ordenada :: (t -> t -> Bool) -> [t] -> Bool
+ordenada _ [] = True
+ordenada _ [x] = True
+ordenada f (x:y:resto) = f x y && ordenada f (y:resto)  
 
 burbuja :: (a->a->Bool) -> [a] -> [a]
 burbuja _ [] = []
 burbuja _ [x] = [x]
 
 burbuja f (x:y:resto)
-    | f x y = (x:burbuja f (y:resto))
-    | otherwise = (y:burbuja f (x:resto))
+    | f x y = x:burbuja f (y:resto)
+    | otherwise = y:burbuja f (x:resto)
